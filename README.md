90e98d953bc2a77d91b825e6773b2734e4510137
ABeloeil <abeloeil@u-pro.fr>
2017-07-06 09:56:21 +0200

    transform to es5

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 #reducer-utilities
+
+Provide some utility functions to manage Redux reducer.
+All update functions return a copy of the provided Array/Object, this does not mutate the initial variable.
+
+### Installation
+
+```bash
+$ yarn add reducer-utilities
+```
+
+### Usage
+
+In your reducer:
+
+```javascript
+//appReducer.js
+
+import {createReducer, updateObject, updateArray, updateItemInArray} from 'reducer-utilities'
+
+const FOO_BAR = 'item.foo_bar'
+
+const reducer = createReducer([] /*our initialState */, {
+  // your actions to handle
+  'update': update,
+  [FOO_BAR]: fooBar,
+})
+
+// A simple map to update an item by his id property. Do not mutate the provided state.
+function update(state, action) {
+  return updateItemInArray(state, action.id, item => {
+    return updateObject(item, {
+      foo: action.foo,
+    })
+  });
+}
+
+function fooBar(state, action) {
+  return updateArray(state, action.items) // return an array instead of updateObject which return an Object.
+}
+
+```
--- index.js
+++ index.js
@@ -1,55 +1,58 @@
-/**
- * Shortcut for Object.assign() which return an object
- *
- * @param oldObject: {}
- * @param newValues: {}
- * @returns {*}
- */
-exports.updateObject = function updateObject(oldObject, newValues) {
-    return Object.assign({}, oldObject, newValues);
-}
+module.exports = {
+  /**
+   * Shortcut for Object.assign() which return an object
+   *
+   * @param oldObject: {}
+   * @param newValues: {}
+   * @returns {*}
+   */
+  updateObject: function (oldObject, newValues) {
+      return Object.assign({}, oldObject, newValues);
+  },
 
-/**
- * Shortcut for Object.assign() which return an array
- *
- * @param oldArray
- * @param newValues
- * @returns {*}
- */
-exports.updateArray = function updateArray(oldArray, newValues) {
-    return Object.assign([], oldArray, newValues);
-}
+  /**
+   * Shortcut for Object.assign() which return an array
+   *
+   * @param oldArray
+   * @param newValues
+   * @returns {*}
+   */
+  updateArray: function (oldArray, newValues) {
+      return Object.assign([], oldArray, newValues);
+  },
 
-/**
- * Update an item in array with custom function
- *
- * @param array: []
- * @param itemId: int
- * @param updateItemCallback: fn()
- * @returns {*}
- */
-exports.updateItemInArray =  function updateItemInArray(array, itemId, updateItemCallback) {
-    return array.map(item => {
-        if (item.id !== itemId) {
-            return item;
-        }
+  /**
+   * Update an item in array with custom function
+   *
+   * @param array: []
+   * @param itemId: int
+   * @param updateItemCallback: fn()
+   * @returns {*}
+   */
+  updateItemInArray: function (array, itemId, updateItemCallback) {
+      return array.map(function(item) {
+          if (item.id !== itemId) {
+              return item;
+          }
 
-        return updateItemCallback(item);
-    });
-}
+          return updateItemCallback(item);
+      });
+  },
 
-/**
- * Create a reducer without using a switch
- *
- * @param initialState: []|{}
- * @param handlers: {}
- */
-exports.createReducer = function createReducer(initialState, handlers) {
-    return function reducer(state = initialState, action) {
-        if (handlers.hasOwnProperty(action.type)) {
-            return handlers[action.type](state, action);
-        } else {
-            return state;
-        }
-    };
+  /**
+   * Create a reducer without using a switch
+   *
+   * @param initialState: []|{}
+   * @param handlers: {}
+   */
+  createReducer: function (initialState, handlers) {
+      return function reducer(state = initialState, action) {
+          if (handlers.hasOwnProperty(action.type)) {
+              return handlers[action.type](state, action);
+          } else {
+              return state;
+          }
+      };
+  }
+
 }
